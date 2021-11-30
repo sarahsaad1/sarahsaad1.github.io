84c3ecc45aa4974d8a063416c5eddde072655e13
sarahsaad1 <89902160+sarahsaad1@users.noreply.github.com>
2021-12-01 02:41:27 +0300

    Add files via upload

---
+++ DPconnect.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang = "en ">
+<head>
+    <title>form-vaildation</title>
+    <meta charset="UTF-8">
+</head>
+
+<body> 
+    <?php 
+    echo "sarah"
+    ?>
+  <h1>SIGN UP</h1>
+  <form >
+      <label for = "fn">Full name:</label>
+      <input type = "text" id = "fn"  name  = "fullName"><span id = "e1"></span>
+      <br><br>
+      
+      <label for = "un">User name:</label>
+      <input type = "text" id = "un"  name  = "userName"><span id = "e2"></span><br><br>
+      
+
+      <label for = "pas">password:</label>
+      <input type = "password" id = "pas"  name  = "password"><span id = "e3"></span><br><br>
+      
+
+      <label for = "cp">Confrim password:</label>
+      <input type = "password" id = "cp"  name  = "passcheck"><span id = "e4"></span><br><br>
+      
+
+      <input type = "submit" value = "SIGN UP">
+
+  </form>
+    </body>
+    </html>
